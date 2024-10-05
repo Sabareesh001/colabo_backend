b9f7cf6b7f01b26472f70284a80572d5108ea7dc
vibez-2k <vibez2055@gmail.com>
2024-10-05 16:21:57 +0530

    goal controller edit

--- src/controllers/goals_controller.js
+++ src/controllers/goals_controller.js
@@ -1,16 +1,21 @@
 const { goals } = require("../../models");
+function ToTitleCase(str) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
 const goalsPost = async (req, res) => {
   const { name, description, start_date, end_date, roadmap_id, tag_id } =
     req.body;
 
+  const TitleCaseName = ToTitleCase(name);
+
   // Check if goal name already exists
   const goalname = await goals.findOne({
     where: {
-      name: name.trim().toLowerCase(),
+      name: TitleCaseName,
     },
   });
-
-
 
   if (goalname) {
     return res.status(403).json({
@@ -19,7 +24,7 @@ const goalsPost = async (req, res) => {
   }
 
   const newgoal = await goals.create({
-    name,
+    name: TitleCaseName,
     description,
     start_date,
     end_date,
