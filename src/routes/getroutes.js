e57153b3d8d6f40a51c1a4439ac9029dd42aa3b9
Suresh-Kannan-8055 <sureshkannangeek@gmail.com>
2024-10-05 10:48:59 +0530

    suresh changes

---
+++ src/routes/getroutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const GetController = require("../controllers/getcontroller");
+
+// router.get("/", GetController.);
+
+module.exports = router;
