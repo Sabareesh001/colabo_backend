865507eaa62f9492d7ade584bf7ec529c45dab9a
Suresh-Kannan-8055 <sureshkannangeek@gmail.com>
2024-10-05 12:06:56 +0530

    changed

--- src/server.js
+++ src/server.js
@@ -1,25 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const sequelize = require('../config');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const sequelize = require("../src/config/index");
 
-const app = express();
+const app = express();  
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const getController=require('./controllers/getcontroller');
-app.use("/api",getController);
+const getController = require("./controllers/getcontroller");
+app.use("/api", getController);
 
-app.get('/', (req, res) => {
-
-    res.send('Hello World!');
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
 sequelize.sync();
